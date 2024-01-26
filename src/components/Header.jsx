6782853f90c80806526eaf3d4198aa321414c4fd
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-01-26 20:27:00 -0300

    Fix logo centering

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <header className="">
+    <header className="mt-2 relative">
         <nav className="flex gap-3 justify-between text-xl z-5">
             <ul className="flex gap-3">
                 <li className="hover:underline decoration-black underline-offset-4"><Link to="/category/women">Women</Link></li>
@@ -12,10 +12,8 @@ const Header = () => {
             <div className="">
                 <Link className="hover:underline decoration-black underline-offset-4" to="/cart">Cart</Link>
             </div>
+            <Link to="/" className="logo absolute left-[50%] m-0 w-30 top-0 z-10 block uppercase tracking-[5px] font-bold text-4xl flex-grow select-none">Svelte</Link>
         </nav>
-        <div className="absolute left-0 right-0 top-4 z-[-99]">
-            <Link to="/" className="block uppercase tracking-[5px] font-bold text-4xl flex-grow text-center select-none">Svelte</Link>
-        </div>
     </header>
   )
 }
