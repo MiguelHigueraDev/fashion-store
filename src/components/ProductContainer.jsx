aa028fd7b8b3e6e5bb0f993b2e0e42c0005c0d0a
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-02-02 20:37:22 -0300

    Fix ProductContainer add qty

--- src/components/ProductContainer.jsx
+++ src/components/ProductContainer.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { PropTypes } from 'prop-types'
+import { PropTypes } from "prop-types";
 import Loading from "./Loading";
 import Error from "./Error";
 import { ShopContext } from "../Router";
@@ -12,8 +12,18 @@ const ProductContainer = ({ productId }) => {
   const [quantity, setQuantity] = useState(1);
 
   const handleInputChange = (e) => {
-    setQuantity(e.target.value)
-  }
+    if (quantity < 1) return setQuantity(1);
+    setQuantity(e.target.value);
+  };
+
+  const increaseQty = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decreaseQty = () => {
+    if (quantity - 1 < 1) return;
+    setQuantity(quantity - 1);
+  };
 
   useEffect(() => {
     setLoading(true);
@@ -26,24 +36,56 @@ const ProductContainer = ({ productId }) => {
       .finally(() => setLoading(false));
   }, [productId]);
 
-  if (loading) return <Loading />
-  if (error) return <Error message={'Product not found'}/>
+  if (loading) return <Loading />;
+  if (error) return <Error message={"Product not found"} />;
 
   return (
     <div className="max-w-800px mx-auto mt-[80px] md:mt-0 md:h-[90vh] flex flex-col md:flex-row gap-10 items-center justify-center">
-        <img src={productObj.image} width={"200px"} height={"200px"} className="" />
-        <div className="max-w-[500px] space-y-4 flex justify-center align-center flex-col">
-            <h3 className="font-bold text-2xl">{productObj.title}</h3>
-            <p className="text-lg font-normal text-gray-600">{productObj.description}</p>
-            <div className="flex gap-3 items-end">
-              <input type="number" min="1" className="rounded-md border-2 border-gray-300 h-10 w-[70px] p-2" value={quantity} onInput={handleInputChange}/>
-              <button className="self-start btn" onClick={() => addToCart(productObj, quantity)}>Add to cart</button>
-            </div>
+      <img
+        src={productObj.image}
+        width={"200px"}
+        height={"200px"}
+        className=""
+      />
+      <div className="max-w-[500px] space-y-4 flex justify-center align-center flex-col">
+        <h3 className="font-bold text-2xl">{productObj.title}</h3>
+        <p className="text-lg font-normal text-gray-600">
+          {productObj.description}
+        </p>
+        <div className="flex gap-3 items-end">
+          <div className="flex gap-2">
+            <button
+              className="rounded-md p-2 bg-gray-200 hover:bg-gray-100 active:bg-gray-100 font-bold text-md w-[30px]"
+              onClick={decreaseQty}
+            >
+              -
+            </button>
+            <input
+              type="number"
+              min="1"
+              className="rounded-md border-2 border-gray-300 h-10 w-[70px] p-2"
+              value={quantity}
+              onInput={handleInputChange}
+            />
+            <button
+              className="rounded-md p-2 bg-gray-200 hover:bg-gray-100 active:bg-gray-100 font-bold text-md w-[30px]"
+              onClick={increaseQty}
+            >
+              +
+            </button>
+          </div>
+          <button
+            className="self-start btn"
+            onClick={() => addToCart(productObj, quantity)}
+          >
+            Add to cart
+          </button>
         </div>
+      </div>
     </div>
-  )
+  );
 };
 ProductContainer.propTypes = {
-    productId: PropTypes.string
-}
+  productId: PropTypes.string,
+};
 export default ProductContainer;
